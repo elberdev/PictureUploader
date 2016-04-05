9903585272afb643f1a378923c34586bf1c2bd4f
elberdev <elberdev@gmail.com>
2016-04-05 19:49:56 -0400

    added a ton of explanatory comments to my simple code. There are more comments that code :/

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+// this first line requires the http module that comes with node.js and
+// makes it available through the variable http
 var http = require("http");
 
+// the createServer function from the http module returns an object with
+// a listen function which takes a port number for our http server to listen
+// on. We did not have to chain the functions this way. We could have
+// put the object in a variable, then later called the listen function from
+// the variable. We are also passing in an anonymous function right away as
+// a value which tells our server what to do and how to respond.
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World!");
