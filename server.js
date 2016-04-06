81ee85f92abd43271c4f39d57369c5d45e9dd89d
elberdev <elberdev@gmail.com>
2016-04-05 20:21:16 -0400

    made server.js into a module and created a file index.js from where to start the app

--- server.js
+++ server.js
@@ -2,28 +2,34 @@
 // makes it available through the variable http
 var http = require("http");
 
-// the createServer function from the http module returns an object with
-// a listen function which takes a port number for our http server to listen
-// on. We did not have to chain the functions this way. We could have
-// put the object in a variable, then later called the listen function from
-// the variable. We are also passing in an anonymous function right away as
-// a value which tells our server what to do and how to respond.
-//
-// http.createServer(function(request, response) {
-//   response.writeHead(200, {"Content-Type": "text/plain"});
-//   response.write("Hello World!");
-//   response.end();
-// }).listen(8888);
+function start() {
+  // the createServer function from the http module returns an object with
+  // a listen function which takes a port number for our http server to listen
+  // on. We did not have to chain the functions this way. We could have
+  // put the object in a variable, then later called the listen function from
+  // the variable. We are also passing in an anonymous function right away as
+  // a value which tells our server what to do and how to respond.
+  //
+  // http.createServer(function(request, response) {
+  //   response.writeHead(200, {"Content-Type": "text/plain"});
+  //   response.write("Hello World!");
+  //   response.end();
+  // }).listen(8888);
 
-// this is the non-anonymous way:
-function onRequest(request, response) {
-  console.log("Request received.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World!");
-  response.end();
+  // this is the non-anonymous way:
+  function onRequest(request, response) {
+    console.log("Request received.");
+    // 200 is the status code, and the second parameter is the content type of
+    // the response
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World!");
+    response.end();
+  }
+
+  // the onRequest function is a callback function (like a completion block)
+  http.createServer(onRequest).listen(8888);
+
+  console.log("Server was started.");
 }
 
-// the onRequest function is a callback function (like a completion block)
-http.createServer(onRequest).listen(8888);
-
-console.log("Server was started.");
+exports.start = start;
