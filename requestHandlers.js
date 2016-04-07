a1dd0462b1f845c8700e2ed7913323813a23e581
elberdev <elberdev@gmail.com>
2016-04-07 08:00:45 -0400

    changed requestHandlers.js to simulate a blocking operation

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,16 @@
 function start() {
   console.log("Request handler 'start' was called.");
+
+  // hack together a sleep function in javascript
+  function sleep(milliseconds) {
+    var startTime = new Date().getTime();
+    // keep this empty loop running while time since 1970 is less
+    // than our target time.
+    while (new Date().getTime() < startTime + milliseconds);
+  }
+
+  // simulate a blocking operation
+  sleep(10000);
   return "Hello Start";
 }
 
