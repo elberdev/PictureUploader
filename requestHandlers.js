0a5e19833867a489b46481a2dfbc4e1da03d9a85
elberdev <elberdev@gmail.com>
2016-04-07 09:12:54 -0400

    added a real nasty html response to be delivered by the start request handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,6 +5,23 @@ var exec = require("child_process").exec;
 function start(response) {
   console.log("Request handler 'start' was called.");
 
+  // UGLY
+  var body = '<html>' +
+             '<head>' +
+             '<meta http-equiv="Content-Type" content="text/html charset=UTF-8" />' +
+             '</head>' +
+             '<body>' +
+             '<form action="/upload" method="post">' +
+             '<textarea name="text" rows="10" cols"60"></textarea>' +
+             '<input type="submit" value="Submit text" />' +
+             '</form>' +
+             '</body>' +
+             '</html>';
+
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(body);
+  response.end();
+
   // // hack together a sleep function in javascript
   // function sleep(milliseconds) {
   //   var startTime = new Date().getTime();
@@ -15,16 +32,16 @@ function start(response) {
   // // simulate a blocking operation
   // sleep(10000);
 
-  // an asynchronous operation
-  // the anonymous function is a callback function to handle the results
-  exec("find /",
-    { timeout: 10000, maxBuffer: 20000 * 1024 },
-    function(error, stdout, stderr) {
-      response.writeHead(200, {"Content-Type": "text/plain"});
-      response.write(stdout);
-      response.end();
-    }
-  );
+  // // an asynchronous operation
+  // // the anonymous function is a callback function to handle the results
+  // exec("find /",
+  //   { timeout: 10000, maxBuffer: 20000 * 1024 },
+  //   function(error, stdout, stderr) {
+  //     response.writeHead(200, {"Content-Type": "text/plain"});
+  //     response.write(stdout);
+  //     response.end();
+  //   }
+  // );
 }
 
 function upload(response) {
