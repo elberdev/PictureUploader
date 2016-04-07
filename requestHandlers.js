3efc3b7a1274c97a2496e9b460297792d891f207
elberdev <elberdev@gmail.com>
2016-04-07 08:39:13 -0400

    changed requestHandlers.js to test a non-blocking operation

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,27 @@
+// import child_process module to test non-blocking operations.
+// exec() executes a shell command
+var exec = require("child_process").exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
+  var content = "empty";
 
-  // hack together a sleep function in javascript
-  function sleep(milliseconds) {
-    var startTime = new Date().getTime();
-    // keep this empty loop running while time since 1970 is less
-    // than our target time.
-    while (new Date().getTime() < startTime + milliseconds);
-  }
+  // // hack together a sleep function in javascript
+  // function sleep(milliseconds) {
+  //   var startTime = new Date().getTime();
+  //   // keep this empty loop running while time since 1970 is less
+  //   // than our target time.
+  //   while (new Date().getTime() < startTime + milliseconds);
+  // }
+  // // simulate a blocking operation
+  // sleep(10000);
 
-  // simulate a blocking operation
-  sleep(10000);
-  return "Hello Start";
+  // the anonymous function is a callback function to handle the results
+  exec("ls -lah", function(error, stdout, stderr) {
+    content = stdout;
+  });
+
+  return content;
 }
 
 function upload() {
