a89b4bb984bd43410a3be8b71b0c436712ebaae8
elberdev <elberdev@gmail.com>
2016-04-09 05:30:09 -0400

    added a show method in requestHandlers to handle showing the uploaded data

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,7 +2,8 @@
 // exec() executes a shell command
 //var exec = require("child_process").exec;
 
-var querystring = require("querystring");
+var querystring = require("querystring"),
+    fs          = require("fs");
 
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
@@ -54,5 +55,12 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  response.writeHead(200, {"Content-Type": "image/png"});
+  fs.createReadStream("/tmp/test/png").pipe(response);
+}
+
 exports.start  = start;
 exports.upload = upload;
+exports.show   = show;
